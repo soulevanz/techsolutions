28ca86f7ab4648c6f8fdd343f83ec9f3dac52abf
willian <willianhndz24@gmail,com>
2025-02-13 02:07:41 -0400

    proyecto de la uni

---
+++ Config/Config.php
@@ -0,0 +1,16 @@
+<?php
+const BASE_URL = "http://localhost/Novi_Final_final/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "techsolutions1";
+const CHARSET = "charset=utf8";
+const TITLE = "TIENDA ONLINE";
+const MONEDA = "USD";
+const CLIENT_ID = "";
+
+const USER_SMTP = "Tu correo";
+const PASS_SMTP = "contraseña generada para esta aplicacion";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
+?>
